24a4d6edecc02acf37dab989d1b2d6aeb14ee5a7
Andre <andre.jemison@gmail.com>
2018-01-17 10:08:30 -0800

    corrected test.py

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import bird_on_a_wire
 
+
 def start_session():
     obj = bird_on_a_wire.Session()
     obj.update_quote()
+    return True
 
 
-stop = None
+if __name__ == '__main__':
+    start_session()
